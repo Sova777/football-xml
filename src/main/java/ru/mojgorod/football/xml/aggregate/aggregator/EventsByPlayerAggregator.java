7cb290f6166189d1f7db3d92c6d45600b268b11e
sova <sova@nm.ru>
2019-08-10 21:13:35 +0300

    Исправил статистику игрока если игрок играл за 2 клуба в сезоне

--- src/main/java/ru/mojgorod/football/xml/aggregate/aggregator/EventsByPlayerAggregator.java
+++ src/main/java/ru/mojgorod/football/xml/aggregate/aggregator/EventsByPlayerAggregator.java
@@ -27,9 +27,16 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 package ru.mojgorod.football.xml.aggregate.aggregator;
 
 import java.io.PrintStream;
+import java.text.Collator;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import ru.mojgorod.football.xml.aggregate.Aggregator;
 import ru.mojgorod.football.xml.library.FootballEventType;
 import ru.mojgorod.football.xml.library.FootballXmlEvent;
@@ -48,60 +55,114 @@ public class EventsByPlayerAggregator extends Aggregator {
 
     @Override
     public void add(FootballXmlReport xmlReport) {
-        TournamentStat stat = TournamentStat.get(playersMap, playerId);
+        String team1 = xmlReport.getTeam1();
+        String teamKey1 = xmlReport.getTeamKey1();
+        String team2 = xmlReport.getTeam2();
+        String teamKey2 = xmlReport.getTeamKey2();
+        int date = xmlReport.getDateInt();
 
         List<FootballXmlPlayer> players1 = xmlReport.getPlayers1();
         for (FootballXmlPlayer player : players1) {
             if (playerId.equals(player.getKey())) {
+                TournamentStat stat = TournamentStat.get(playersMap, teamKey1);
                 stat.games++;
+                stat.team = team1;
             }
         }
 
         List<FootballXmlPlayer> players2 = xmlReport.getPlayers2();
         for (FootballXmlPlayer player : players2) {
             if (playerId.equals(player.getKey())) {
+                TournamentStat stat = TournamentStat.get(playersMap, teamKey2);
                 stat.games++;
+                stat.team = team2;
             }
         }
 
         List<FootballXmlEvent> events = xmlReport.getEvents();
         for (FootballXmlEvent event : events) {
+            String team = event.getTeam();
+            String teamKey;
+            if (team.equals(team1)) {
+                teamKey = teamKey1;
+            } else if (team.equals(team2)) {
+                teamKey = teamKey2;
+            } else {
+                Logger.getLogger(EventsByPlayerAggregator.class.getName()).log(Level.SEVERE, "Unknown team: {0}", team);
+                continue;
+            }
+
             FootballEventType eventType = event.getEventType();
             switch (eventType) {
                 case SUBSTITUTION:
                     if (playerId.equals(event.getPlayerKey2())) {
+                        TournamentStat stat = TournamentStat.get(playersMap, teamKey);
+                        stat.team = team;
+                        if (stat.firstGame > date) {
+                            stat.firstGame = date;
+                        }
                         stat.games++;
                     }
                     break;
                 case GOAL:
                     if (playerId.equals(event.getPlayerKey1())) {
+                        TournamentStat stat = TournamentStat.get(playersMap, teamKey);
+                        stat.team = team;
+                        if (stat.firstGame > date) {
+                            stat.firstGame = date;
+                        }
                         stat.goals++;
                     }
                     break;
                 case PENALTY_GOAL:
                     if (playerId.equals(event.getPlayerKey1())) {
+                        TournamentStat stat = TournamentStat.get(playersMap, teamKey);
+                        stat.team = team;
+                        if (stat.firstGame > date) {
+                            stat.firstGame = date;
+                        }
                         stat.goals++;
                         stat.penalty++;
                     }
                     break;
                 case PENALTY_MISSED:
                     if (playerId.equals(event.getPlayerKey1())) {
+                        TournamentStat stat = TournamentStat.get(playersMap, teamKey);
+                        stat.team = team;
+                        if (stat.firstGame > date) {
+                            stat.firstGame = date;
+                        }
                         stat.penaltyMissed++;
                     }
                     break;
                 case AUTOGOAL:
                     if (playerId.equals(event.getPlayerKey1())) {
+                        TournamentStat stat = TournamentStat.get(playersMap, teamKey);
+                        stat.team = team;
+                        if (stat.firstGame > date) {
+                            stat.firstGame = date;
+                        }
                         stat.autogoal++;
                     }
                     break;                        
                 case RED_AND_YELLOW_CARD:
                 case RED_CARD:
                     if (playerId.equals(event.getPlayerKey1())) {
+                        TournamentStat stat = TournamentStat.get(playersMap, teamKey);
+                        stat.team = team;
+                        if (stat.firstGame > date) {
+                            stat.firstGame = date;
+                        }
                         stat.redCards++;
                     }
                     break;
                 case YELLOW_CARD:
                     if (playerId.equals(event.getPlayerKey1())) {
+                        TournamentStat stat = TournamentStat.get(playersMap, teamKey);
+                        stat.team = team;
+                        if (stat.firstGame > date) {
+                            stat.firstGame = date;
+                        }
                         stat.yellowCards++;
                     }
                     break;
@@ -111,17 +172,23 @@ public class EventsByPlayerAggregator extends Aggregator {
 
     @Override
     public void afterSeason() {
-        SeasonStat seasonStat = new SeasonStat();
-        TournamentStat tournamentStat = playersMap.get(playerId);
-        seasonStat.games = tournamentStat.games;
-        seasonStat.goals = tournamentStat.goals;
-        seasonStat.penalty = tournamentStat.penalty;
-        seasonStat.penaltyMissed = tournamentStat.penaltyMissed;
-        seasonStat.autogoal = tournamentStat.autogoal;
-        seasonStat.redCards = tournamentStat.redCards;
-        seasonStat.yellowCards = tournamentStat.yellowCards;
-        seasonStat.title = getSeason().getTitle();
-        seasons.add(seasonStat);
+        TreeMap<String, TournamentStat> sortedMap = new TreeMap<>(new StatComparator(playersMap));
+        sortedMap.putAll(playersMap);
+        for (Map.Entry<String, TournamentStat> entry : sortedMap.entrySet()) {
+            TournamentStat tournamentStat = entry.getValue();
+            SeasonStat seasonStat = new SeasonStat();
+            seasonStat.team = tournamentStat.team;
+            seasonStat.firstGame = tournamentStat.firstGame;
+            seasonStat.games = tournamentStat.games;
+            seasonStat.goals = tournamentStat.goals;
+            seasonStat.penalty = tournamentStat.penalty;
+            seasonStat.penaltyMissed = tournamentStat.penaltyMissed;
+            seasonStat.autogoal = tournamentStat.autogoal;
+            seasonStat.redCards = tournamentStat.redCards;
+            seasonStat.yellowCards = tournamentStat.yellowCards;
+            seasonStat.title = getSeason().getTitle();
+            seasons.add(seasonStat);
+        }
         playersMap.clear();
     }
 
@@ -129,10 +196,10 @@ public class EventsByPlayerAggregator extends Aggregator {
         PrintStream out = getOutputFinalReport();
         out.println("<h2 id='EventsByPlayerAggregator'>Статистика за сезон</h2>");
         out.println("<pre>");
-        out.println("==================================================================================================");
-        out.println("| Сезон           | Матчей     | Мячей      | Незабитые  | Забито в    | Удалений   | Предупре-  |");
-        out.println("|                 |            |(с пенальти)| пенальти   | свои ворота |            | ждений     |");
-        out.println("==================================================================================================");
+        out.println("====================================================================================================================");
+        out.println("| Сезон           | Команда         | Матчей     | Мячей      | Незабитые  | Забито в    | Удалений   | Предупре-  |");
+        out.println("|                 |                 |            |(с пенальти)| пенальти   | свои ворота |            | ждений     |");
+        out.println("====================================================================================================================");
         int games = 0;
         int goals = 0;
         int penalty = 0;
@@ -152,23 +219,24 @@ public class EventsByPlayerAggregator extends Aggregator {
                 String seasonGoals = (season.penalty > 0)
                         ? String.valueOf(season.goals) + "(" + season.penalty + ")"
                         : String.valueOf(season.goals);
-                out.printf("| %-15s | %-10d | %-10s | %-10d | %-11d | %-10d | %-10d |%n",
-                        season.title, season.games, seasonGoals, season.penaltyMissed, season.autogoal, season.redCards, season.yellowCards);
+                out.printf("| %-15s | %-15s | %-10d | %-10s | %-10d | %-11d | %-10d | %-10d |%n",
+                        season.title, season.team, season.games, seasonGoals, season.penaltyMissed, season.autogoal, season.redCards, season.yellowCards);
             }
         }
-        out.println("--------------------------------------------------------------------------------------------------");
+        out.println("-------------------------------------------------------------------------------------------------------------------");
         String allGoals = (penalty > 0)
                 ? String.valueOf(goals) + "(" + penalty + ")"
                 : String.valueOf(goals);
-        out.printf("| %-15s | %-10d | %-10s | %-10d | %-11d | %-10d | %-10d |%n",
-                "Итого", games, allGoals, penaltyMissed, autogoal, redCards, yellowCards);
-        out.println("==================================================================================================");
+        out.printf("| %-15s | %-15s | %-10d | %-10s | %-10d | %-11d | %-10d | %-10d |%n",
+                "Итого", "", games, allGoals, penaltyMissed, autogoal, redCards, yellowCards);
+        out.println("====================================================================================================================");
         out.println( "</pre>");
     }
 
     static private class SeasonStat {
 
         private String title = "";
+        private String team = "";
         private int games = 0;
         private int goals = 0;
         private int penalty = 0;
@@ -176,6 +244,7 @@ public class EventsByPlayerAggregator extends Aggregator {
         private int autogoal = 0;
         private int yellowCards = 0;
         private int redCards = 0;
+        private int firstGame = Integer.MAX_VALUE;
 
     }
 
@@ -188,15 +257,42 @@ public class EventsByPlayerAggregator extends Aggregator {
         private int autogoal = 0;
         private int yellowCards = 0;
         private int redCards = 0;
-        private HashMap<String, Integer> players = new HashMap<>();
+        private int firstGame = Integer.MAX_VALUE;
+        private String team = "";
+        private HashMap<String, Integer> teams = new HashMap<>();
 
         public static TournamentStat get(final HashMap<String, TournamentStat> hashStat, final String keyStat) {
             if (!hashStat.containsKey(keyStat)) {
                 TournamentStat stat = new TournamentStat();
-                stat.players = new HashMap<>();
+                stat.teams = new HashMap<>();
                 hashStat.put(keyStat, stat);
             }
             return hashStat.get(keyStat);
+        }
+
+    }
+
+    static private class StatComparator implements Comparator<String> {
+
+        Collator collator = Collator.getInstance(new Locale("ru", "RU"));
+        private final HashMap<String, TournamentStat> map;
+
+        public StatComparator(final HashMap<String, TournamentStat> map) {
+            this.map = map;
+        }
+
+        @Override
+        public int compare(String key1, String key2) {
+            TournamentStat stat1 = map.get(key1);
+            TournamentStat stat2 = map.get(key2);
+            int date1 = stat1.firstGame;
+            int date2 = stat2.firstGame;
+            if (date1 < date2) {
+                return -1;
+            } else if (date1 > date2) {
+                return 1;
+            }
+            return collator.compare(stat1.team, stat2.team);
         }
 
     }
